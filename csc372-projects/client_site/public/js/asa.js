713d8f3a2230013dca82c2f7c349df64f0e9dcfa
selenityyy <selenac17@icloud.com>
2026-03-08 21:53:50 -0400

    added php class/objects

--- csc372-projects/client_site/public/js/asa.js
+++ csc372-projects/client_site/public/js/asa.js
@@ -40,5 +40,5 @@ fetchEvents();
 
 // goes back to homepage when logo is clicked
 function goHome() {
-    window.location.href = '/';
+    window.location.href = 'http://localhost/document-root-folder/client_site/public/index.php';
 }
--- csc372-projects/client_site/public/js/error.js
+++ csc372-projects/client_site/public/js/error.js
@@ -1,3 +1,3 @@
 function goHome() {
-    window.location.href = '/';
+    window.location.href = 'http://localhost/document-root-folder/client_site/public/index.php';
 }
